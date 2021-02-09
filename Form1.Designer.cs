253d994bf8a2a615e0a9c201bb962ce0f8051059
Savio Franses <saviofranses@gmail.com>
2021-02-09 14:23:55 +0100

    poängsystem och studslogik

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -30,100 +30,103 @@ namespace agiltpong
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
-            this.pictureBox2 = new System.Windows.Forms.PictureBox();
-            this.pictureBox3 = new System.Windows.Forms.PictureBox();
-            this.timer1 = new System.Windows.Forms.Timer(this.components);
-            this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            this.pongtimer = new System.Windows.Forms.Timer(this.components);
+            this.Spelare = new System.Windows.Forms.Label();
+            this.boll = new System.Windows.Forms.PictureBox();
+            this.Motståndare = new System.Windows.Forms.Label();
+            this.platta = new System.Windows.Forms.PictureBox();
+            this.platta2 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.boll)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.platta)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.platta2)).BeginInit();
             this.SuspendLayout();
             // 
-            // pictureBox1
+            // pongtimer
             // 
-            this.pictureBox1.BackColor = System.Drawing.Color.Blue;
-            this.pictureBox1.Location = new System.Drawing.Point(53, 211);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(44, 232);
-            this.pictureBox1.TabIndex = 0;
-            this.pictureBox1.TabStop = false;
-            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            this.pongtimer.Enabled = true;
+            this.pongtimer.Interval = 20;
+            this.pongtimer.Tick += new System.EventHandler(this.timerTick);
             // 
-            // pictureBox2
+            // Spelare
             // 
-            this.pictureBox2.BackColor = System.Drawing.Color.Red;
-            this.pictureBox2.Location = new System.Drawing.Point(1304, 211);
-            this.pictureBox2.Name = "pictureBox2";
-            this.pictureBox2.Size = new System.Drawing.Size(44, 232);
-            this.pictureBox2.TabIndex = 1;
-            this.pictureBox2.TabStop = false;
+            this.Spelare.AutoSize = true;
+            this.Spelare.BackColor = System.Drawing.Color.Black;
+            this.Spelare.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F);
+            this.Spelare.ForeColor = System.Drawing.Color.Cyan;
+            this.Spelare.Location = new System.Drawing.Point(105, 9);
+            this.Spelare.Name = "Spelare";
+            this.Spelare.Size = new System.Drawing.Size(36, 26);
+            this.Spelare.TabIndex = 3;
+            this.Spelare.Text = "00";
             // 
-            // pictureBox3
+            // boll
             // 
-            this.pictureBox3.BackColor = System.Drawing.SystemColors.ButtonHighlight;
-            this.pictureBox3.Location = new System.Drawing.Point(653, 308);
-            this.pictureBox3.Name = "pictureBox3";
-            this.pictureBox3.Size = new System.Drawing.Size(28, 28);
-            this.pictureBox3.TabIndex = 2;
-            this.pictureBox3.TabStop = false;
+            this.boll.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.boll.Location = new System.Drawing.Point(434, 239);
+            this.boll.Name = "boll";
+            this.boll.Size = new System.Drawing.Size(27, 26);
+            this.boll.TabIndex = 2;
+            this.boll.TabStop = false;
             // 
-            // timer1
+            // Motståndare
             // 
-            this.timer1.Interval = 20;
+            this.Motståndare.AutoSize = true;
+            this.Motståndare.BackColor = System.Drawing.Color.Black;
+            this.Motståndare.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F);
+            this.Motståndare.ForeColor = System.Drawing.Color.Cyan;
+            this.Motståndare.Location = new System.Drawing.Point(735, 9);
+            this.Motståndare.Name = "Motståndare";
+            this.Motståndare.Size = new System.Drawing.Size(36, 26);
+            this.Motståndare.TabIndex = 4;
+            this.Motståndare.Text = "00";
             // 
-            // label1
+            // platta
             // 
-            this.label1.AutoSize = true;
-            this.label1.BackColor = System.Drawing.Color.Blue;
-            this.label1.Location = new System.Drawing.Point(230, 50);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(19, 13);
-            this.label1.TabIndex = 3;
-            this.label1.Text = "00";
-            this.label1.Click += new System.EventHandler(this.label1_Click);
+            this.platta.BackColor = System.Drawing.Color.Cyan;
+            this.platta.Location = new System.Drawing.Point(12, 230);
+            this.platta.Name = "platta";
+            this.platta.Size = new System.Drawing.Size(26, 127);
+            this.platta.TabIndex = 0;
+            this.platta.TabStop = false;
             // 
-            // label2
+            // platta2
             // 
-            this.label2.AutoSize = true;
-            this.label2.BackColor = System.Drawing.Color.Blue;
-            this.label2.Location = new System.Drawing.Point(1056, 50);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(19, 13);
-            this.label2.TabIndex = 4;
-            this.label2.Text = "00";
+            this.platta2.BackColor = System.Drawing.Color.Red;
+            this.platta2.Location = new System.Drawing.Point(889, 230);
+            this.platta2.Name = "platta2";
+            this.platta2.Size = new System.Drawing.Size(27, 127);
+            this.platta2.TabIndex = 1;
+            this.platta2.TabStop = false;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
-            this.ClientSize = new System.Drawing.Size(1385, 760);
-            this.Controls.Add(this.label2);
-            this.Controls.Add(this.label1);
-            this.Controls.Add(this.pictureBox3);
-            this.Controls.Add(this.pictureBox2);
-            this.Controls.Add(this.pictureBox1);
-            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.ClientSize = new System.Drawing.Size(928, 574);
+            this.Controls.Add(this.Motståndare);
+            this.Controls.Add(this.Spelare);
+            this.Controls.Add(this.boll);
+            this.Controls.Add(this.platta2);
+            this.Controls.Add(this.platta);
+            this.Margin = new System.Windows.Forms.Padding(2);
             this.Name = "Form1";
             this.Text = "Form1";
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.boll)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.platta)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.platta2)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
         }
 
         #endregion
-
-        private System.Windows.Forms.PictureBox pictureBox1;
-        private System.Windows.Forms.PictureBox pictureBox2;
-        private System.Windows.Forms.PictureBox pictureBox3;
-        private System.Windows.Forms.Timer timer1;
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Timer pongtimer;
+        private System.Windows.Forms.Label Spelare;
+        private System.Windows.Forms.PictureBox boll;
+        private System.Windows.Forms.Label Motståndare;
+        private System.Windows.Forms.PictureBox platta;
+        private System.Windows.Forms.PictureBox platta2;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -12,18 +12,82 @@ namespace agiltpong
 {
     public partial class Form1 : Form
     {
+        int bollx = 5;
+        int bolly = 5;
+        int EnemyScore = 0;
+        int PlayerScore = 0;
+        int speed = 5;
+        bool goup;
+        bool godown;
         public Form1()
         {
             InitializeComponent();
         }
 
-        private void pictureBox1_Click(object sender, EventArgs e)
+        private void timerTick(object sender, EventArgs e)
         {
+            Spelare.Text = "" + PlayerScore;
+            Motståndare.Text = "" + EnemyScore;
 
-        }
+            boll.Top -= bolly;
+            boll.Left -= bollx;
 
-        private void label1_Click(object sender, EventArgs e)
-        {
+            platta2.Top += speed;
+
+            if (PlayerScore < 5)
+            {
+                if (platta2.Top < 0 || platta2.Top > 455)
+                {
+                    speed = -speed;
+                }
+            }
+            else
+            {
+                platta2.Top = boll.Top + 30;
+            }
+
+            if (boll.Left < 0)
+            {
+                boll.Left = 434;
+                bollx = -bollx;
+                bollx -= 2;
+                EnemyScore++;
+            }
+
+            if (boll.Left + boll.Width > ClientSize.Width)
+            {
+                boll.Left = 434;
+                bollx = -bollx;
+                bollx += 2;
+                PlayerScore++;
+            }
+
+            if (boll.Top < 0 || boll.Top + boll.Height > ClientSize.Height)
+            {
+                bolly = -bolly;
+            }
+
+            if (boll.Bounds.IntersectsWith(platta.Bounds) || boll.Bounds.IntersectsWith(platta2.Bounds))
+            {
+                bollx = -bollx;
+            }
+
+            if (goup == true && platta.Top > 0)
+            {
+                platta.Top -= 8;
+
+            }
+
+            if (godown == true && platta.Top < 455)
+            {
+                platta.Top += 8;
+            }
+
+            if (PlayerScore > 10)
+            {
+                pongtimer.Stop();
+                MessageBox.Show("You win this game");
+            }
 
         }
     }
